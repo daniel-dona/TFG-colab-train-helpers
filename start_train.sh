50a671a054772e69a18f80aaaa90b667e338c1ee
Daniel Doña <daniel.dona@xnor.ga>
2022-04-23 20:56:33 +0200

    Fix

--- start_train.sh
+++ start_train.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+RUN_DIR=$(rclone --config=/content/rclone.conf lsf UPM:/Training/vits_ljs --max-depth 1 | grep -v "phoneme_cache")
+
+
 cd /content/repo/new-recipes
 
 source /content/venv/bin/activate
 
-nohup python3 /content/repo/new-recipes/train_vits_colab.py --continue_path /content/persistence/ 2>&1 > /content/logs/run.log &
+nohup python3 /content/repo/new-recipes/train_vits_colab.py --continue_path /content/persistence/$RUN_DIR 2>&1 > /content/logs/run.log &
