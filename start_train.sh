dafdd165da98ebd1d583db8211ec17a5059097b9
Daniel Doña <daniel.dona@xnor.ga>
2022-04-23 20:54:13 +0200

    Download last

--- start_train.sh
+++ start_train.sh
@@ -4,4 +4,4 @@ cd /content/repo/new-recipes
 
 source /content/venv/bin/activate
 
-nohup python3 /content/coqui-ai-TTS/new-recipes/train_vits_colab.py --continue_path /content/persistence/vits_colab_ljs_sync/ 2>&1 > /content/logs/run.log &
+nohup python3 /content/repo/new-recipes/train_vits_colab.py --continue_path /content/persistence/ 2>&1 > /content/logs/run.log &
--- sync.sh
+++ sync.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+RUN_DIR=$(rclone --config=/content/rclone.conf lsf UPM:/Training/vits_ljs --max-depth 1 | grep -v "phoneme_cache")
+
 while true
 do
 
-	rclone  --config=/content/rclone.conf copy -v --transfers=10 /content/persistence/ UPM_drive:/Training/vits_ljs/ 2>&1 > /content/logs/sync.log
+	rclone  --config=/content/rclone.conf copy -v --transfers=10 /content/persistence/$RUN_DIR UPM_drive:/Training/vits_ljs/$RUN_DIR 2>&1 > /content/logs/sync.log
 	sleep 5m
 
 done
